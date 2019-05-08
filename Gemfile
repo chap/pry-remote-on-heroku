2af7be47f20b549e2c34d5e508419bdf423299c4
Chap Ambrose <chapambrose@gmail.com>
2019-05-08 13:46:11 -0500

    first commit

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source 'https://rubygems.org'
+ruby '2.5.3'
+
+gem 'sinatra'
+gem 'puma'
+gem 'pry-remote'
---
+++ app.rb
@@ -0,0 +1,12 @@
+require "sinatra"
+
+get '/*' do
+  puts params.to_s
+  binding.remote_pry
+  return params.to_s
+end
+
+post '/*' do
+  puts params.to_s
+  return params.to_s
+end
